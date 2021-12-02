402fcc5e7d5a9a116579c04bc29fc84ab5268c0a
Colin Douglas <colindouglas@gmail.com>
2021-12-02 11:49:27 -0400

    add days 1 and 2

---
+++ day1.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+input <- read_csv("day1-input.txt", col_names = FALSE)[[1]]
+
+sum(lead(input, 1) > input, na.rm = TRUE)
+
+# Part 2
+
+sum(zoo::rollsum(lead(input, 1), 3) > zoo::rollsum(input, 3), na.rm = TRUE)
---
+++ day2.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+
+input <- read_csv("day2-input.txt", col_names = FALSE)[[1]]
+
+forward <- input[str_detect(input, "forward")]  %>%
+  str_replace("forward ", "") %>%
+  as.numeric()
+
+updown <- input[!str_detect(input, "forward")] %>%
+  str_replace("up ", "-") %>%
+  str_replace("down ", "") %>%
+  as.numeric()
+
+sum(forward) * sum(updown)  
+
+
+# Part 2
+
+
+# down X increases your aim by X units.
+# up X decreases your aim by X units.
+# forward X does two things:
+#   It increases your horizontal position by X units.
+#   It increases your depth by your aim multiplied by X.
+
+
+
+df_input <- tibble(command = input) %>%
+  separate(command, into = c("dir", "mag")) %>%
+  mutate(
+    mag = as.numeric(mag),
+    delta_aim = case_when(
+      dir == "down" ~ mag, 
+      dir == "up" ~ -mag,
+      TRUE ~ 0),
+    aim = cumsum(delta_aim))
+
+df_pos <- df_input %>%
+  filter(dir == "forward") %>%
+  mutate(pos_hor = cumsum(mag),
+         pos_ver = cumsum(mag * aim))
+
+last(df_pos$pos_hor) * last(df_pos$pos_ver)
